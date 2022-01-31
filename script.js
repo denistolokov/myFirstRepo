9934b05bc4e2b09fd015e80a5f70fee669d7b97b
Den Tolokov <den.tolokov@yandex.ru>
2022-01-31 19:39:21 +0300

    переменные выведены в консоль

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
-let title;
-let screens;
-let screenPrice;
-let roolback;
-let fullPrice;
-let adaptive;
+let title = "My first project";
+console.log(typeof title);
+
+let screens = "Простые, Сложные, Интерективные";
+console.log(screens.length);
+let lowCase = screens.toLowerCase().split(',');
+console.log(lowCase);
+
+let screenPrice = 1000;
+//непойму логику этой задачи, написать стоимость в массиве и вывести его в консоль?
+console.log(screenPrice);
+
+let rollback = 15;
+
+let fullPrice = 10000;
+console.log(typeof fullPrice);
+//процент посреднику
+let servicePercentPrice = fullPrice * (rollback / 100);
+console.log(servicePercentPrice);
+
+let adaptive = true;
+console.log(typeof adaptive);
 
 
-alert('Hello World!')
-console.log('Hello World!')
