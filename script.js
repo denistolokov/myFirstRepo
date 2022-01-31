d28daa63dfea2a6846a596c27fb2624779a4e7d6
Den Tolokov <den.tolokov@yandex.ru>
2022-01-31 19:51:26 +0300

    Исправил выводы в консоль

--- script.js
+++ script.js
@@ -1,24 +1,17 @@
 let title = "My first project";
-console.log(typeof title);
-
 let screens = "Простые, Сложные, Интерективные";
-console.log(screens.length);
-let lowCase = screens.toLowerCase().split(',');
-console.log(lowCase);
-
 let screenPrice = 1000;
-//непойму логику этой задачи, написать стоимость в массиве и вывести его в консоль?
-console.log(screenPrice);
-
 let rollback = 15;
-
 let fullPrice = 10000;
-console.log(typeof fullPrice);
-//процент посреднику
-let servicePercentPrice = fullPrice * (rollback / 100);
-console.log(servicePercentPrice);
-
 let adaptive = true;
+
+
+console.log(typeof title);
+console.log(typeof fullPrice);
 console.log(typeof adaptive);
-
-
+console.log(screens.length);
+console.log(screens.toLowerCase().split(','));
+console.log("Стоимость верстки экранов " + screenPrice + " рублей");
+console.log("стоимость разработки сайта " + fullPrice + " рублей")
+//процент посреднку
+console.log(fullPrice * (rollback / 100));
