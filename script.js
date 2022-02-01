fca42989ec45f97a5120db1e01d53810f033094c
Den Tolokov <den.tolokov@yandex.ru>
2022-02-01 21:16:19 +0300

    Написанны условия, проверенна консоль

--- script.js
+++ script.js
@@ -2,15 +2,16 @@
 
 let title = prompt('Как называется ваш проект');
 let screens = prompt('Простые, Сложные, Интерективные');
-let screenPrice = prompt('Сколько будет стоить данная работа');
+let screenPrice = +prompt('Сколько будет стоить данная работа');
 let rollback = 15;
 let adaptive = confirm('Нужен ли адвптив на сайте');
 let service1 = prompt('Какой дополнительный тип услуги нужен?');
-let servicePrice1 = prompt('Сколько это будет стоить?');
+let servicePrice1 = +prompt('Сколько это будет стоить?');
 let service2 = prompt('Какой дополнительный тип услуги нужен?');
-let servicePrice2 = prompt('Сколько это будет стоить?');
+let servicePrice2 = +prompt('Сколько это будет стоить?');
 let fullPrice = screenPrice + servicePrice1 + servicePrice2;
-let servicePercentPrice = fullPrice - (rollback / 100);
+let servicePercentPrice = Math.ceil(fullPrice * (rollback / 100));
+
 console.log(title);
 console.log(screens);
 console.log(screenPrice);
@@ -21,3 +22,13 @@ console.log(service2);
 console.log(servicePrice2);
 console.log(fullPrice);
 console.log(servicePercentPrice);
+
+if (fullPrice >= 30000) {
+    alert('Даём скидку в 10%');
+} else if (fullPrice >= 15000 && fullPrice < 30000) {
+    alert('Даём скидку в 5%');
+} else if (fullPrice < 15000 && fullPrice > 0) {
+    alert('Скидка не предусмотрена');
+} else if (fullPrice <= 0) {
+    alert('Что-то пошло не так');
+}
