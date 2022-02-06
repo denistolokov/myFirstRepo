d17fc3674e471018d91202c876be607e16194ea8
Den Tolokov <den.tolokov@yandex.ru>
2022-02-06 20:07:59 +0300

    Выведение рандомного и пользовательского числа

--- script.js
+++ script.js
@@ -1,1 +1,48 @@
-console.log('Hello World!');
+'use strict';
+
+let mainNumber;
+let userNumber;
+
+const isNumber = function (num) {
+    return !isNaN(parseFloat(num)) && isFinite(num);
+}
+
+const gameBot = function () {
+    mainNumber = Math.floor(Math.random() * 101) + 1;
+    console.log(mainNumber);
+
+    function randomNumber() {
+        userNumber = +prompt("Угадай число от 1 до 100");
+
+        if (userNumber < 1 || userNumber > 100 || userNumber === isNumber) {
+            randomNumber();
+        }
+
+        return userNumber;
+
+    }
+
+    randomNumber();
+}
+
+gameBot();
+
+console.log(userNumber);
+
+// function one(x) {
+//     function two() {
+//         const a = +prompt("Введите число");
+//         x--;
+
+//         console.log(a);
+//         console.log(x);
+
+//         if (a !== x) {
+//             two();
+//         }
+//     }
+
+//     two()
+// }
+
+// one(10);
