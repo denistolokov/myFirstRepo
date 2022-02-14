0e22ae150b1995bf0e09c0268e9f09f9ebbc65fc
Den Tolokov <den.tolokov@yandex.ru>
2022-02-14 21:59:45 +0300

    Выполнил задания для кнопок

--- script1.js
+++ script1.js
@@ -1,12 +1,19 @@
+'use strick'
+
+const buttonColor = document.getElementById("btn");
+const imputGhangeColor = document.querySelector('input[type = "text"]');
+
+const changeColor = function (event) {
+    buttonColor.style.backgroundColor = event.target.value;
+};
+
+buttonColor.addEventListener("click", changeColor);
+imputGhangeColor.addEventListener("input", changeColor);
+
+const displayButton = document.getElementById("e_btn");
+displayButton.style = "display: none;";
 
 
-
-
-
-
-
-const buttonColor = document.getElementById('btn');
-console.log(buttonColor);
 
 
 
