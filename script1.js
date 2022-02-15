74ca46cd08299c7b300ad683877f79f5ee2ce814
Den Tolokov <den.tolokov@yandex.ru>
2022-02-15 18:44:45 +0300

    Выполнил изменения круга

--- script1.js
+++ script1.js
@@ -1,20 +1,29 @@
 'use strick'
-
+// меняем цвет кнопки через input.
 const buttonColor = document.getElementById("btn");
-const imputGhangeColor = document.querySelector('input[type = "text"]');
+const inputChangeColor = document.querySelector('input[type = "text"]');
 
 const changeColor = function (event) {
     buttonColor.style.backgroundColor = event.target.value;
 };
 
 buttonColor.addEventListener("click", changeColor);
-imputGhangeColor.addEventListener("input", changeColor);
+inputChangeColor.addEventListener("input", changeColor);
 
+//удаляем кнопку внутри круга.
 const displayButton = document.getElementById("e_btn");
 displayButton.style = "display: none;";
 
+//Меняем height и width circle
+const inputRange = document.querySelector('input[type = "range"]');
+console.log(inputRange);
+const circle = document.querySelector("#circle");
 
+const changeWidth = function () {
+    circle.style.width = inputRange.value + '%';
+    circle.style.height = inputRange.value + '%';
+};
+changeWidth();
 
-
-
-
+inputRange.addEventListener("input", changeWidth);
+circle.addEventListener("input", changeWidth);
